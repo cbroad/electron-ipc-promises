72e48a7026633721814c42cf543004dffafd8a68
Carl Broadbent <carl.broadbent@gmail.com>
2022-05-02 19:26:39 -0500

    changed scope of some variables and type of error

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ type IPCMessengerResponseData = { err: NodeJS.ErrnoException|string|null, res?: 
 
 type IPCMessengerWaiting = { count:number, entries: { [key: number]: IPCMessengerWaitingEntry } };
 
-type IPCMessengerWaitingEntry = { destroyListener?: ()=>void, reject: (reason?:unknown)=>void, resolve: (value:unknown)=>void, timer: NodeJS.Timeout, };
+type IPCMessengerWaitingEntry = { destroyListener?: ()=>void, reject: (reason?:unknown)=>void, resolve: (value:unknown)=>void, timer?: NodeJS.Timeout, };
 
 
 
@@ -26,21 +26,21 @@ class AbstractIPCMessenger extends EventEmitter {
 	debug: boolean = Config.debug;
 	defaultTimeout: number = Config.timeout;
 	
-	electronIPC: ( Electron.IpcMain | Electron.IpcRenderer );
+	#electronIPC: ( Electron.IpcMain | Electron.IpcRenderer );
 
-	waiting: IPCMessengerWaiting  = { count: 0, entries: {} };
+	#waiting: IPCMessengerWaiting  = { count: 0, entries: {} };
 
 
 	constructor( electronIPC: (Electron.IpcMain | Electron.IpcRenderer) ) {
 		super();
-		this.electronIPC = electronIPC ;
+		this.#electronIPC = electronIPC ;
 
 
-		electronIPC.on( this.channel, ( event: (Electron.IpcMainEvent | Electron.IpcRendererEvent), ...args: any[] ): void => {
+		this.#electronIPC.on( this.channel, ( event: (Electron.IpcMainEvent | Electron.IpcRendererEvent), ...args: any[] ): void => {
 			const { data, label, id } = args[0] as IPCMessengerArg;
 
 			if( label === "response" ) {
-				this.onResponse( id, data as IPCMessengerResponseData );
+				this.#onResponse( id, data as IPCMessengerResponseData );
 			} else {
 				this.debug && this.console.debug( `IPCMessenger.on( "${label}", { id: ${id}, data: ${JSON.stringify(data)} } )` );
 				
@@ -52,7 +52,7 @@ class AbstractIPCMessenger extends EventEmitter {
 					try {
 						event.sender.send( this.channel, payload );
 					} catch( e ) {
-						if( e.message !== "Object has been destroyed" ) {
+						if( ( e as NodeJS.ErrnoException ).message !== "Object has been destroyed" ) {
 							throw e;
 						}
 						this.debug && this.console.debug( "IPCMessenger: Target window has been closed." );
@@ -63,24 +63,24 @@ class AbstractIPCMessenger extends EventEmitter {
 	}
 
 
-	cleanUpAfterMessage( id: number ) {
+	#cleanUpAfterMessage( id: number ) {
 		// this.console.log( `IPCMessenger.cleanupAfterMessage( ${id} )` );
-		if( this.waiting.entries[ id ] ) {
-			const entry = this.waiting.entries[ id ];
+		if( this.#waiting.entries[ id ] ) {
+			const entry = this.#waiting.entries[ id ];
 			if( entry.destroyListener ) {
 				entry.destroyListener();
 			}
 			if( entry.timer ) {
 				clearTimeout( entry.timer );
 			}
-			delete this.waiting.entries[ id ];
+			delete this.#waiting.entries[ id ];
 		}
 	}
 
-	onResponse( id: number, data: IPCMessengerResponseData ): void {
+	#onResponse( id: number, data: IPCMessengerResponseData ): void {
 		this.debug && this.console.debug( `IPCMessenger.onResponse( ${id}, ${JSON.stringify(data)} )` );
 
-		const entry = this.waiting.entries[ id ];
+		const entry = this.#waiting.entries[ id ];
 
 		if( ! entry ) {
 			throw new Error( `Recieved unexpected response: ${ JSON.stringify( { id, data } ) }` );
@@ -92,7 +92,7 @@ class AbstractIPCMessenger extends EventEmitter {
 		} else {
 			resolve( data.res );
 		}
-		this.cleanUpAfterMessage( id );
+		this.#cleanUpAfterMessage( id );
 	}
 
 
@@ -103,31 +103,31 @@ class AbstractIPCMessenger extends EventEmitter {
 	protected async _send( target: Electron.WebContents, label: string, data: IPCMessengerRequestData ): Promise<any>;
 	protected async _send( target: Electron.WebContents, label: string, data: IPCMessengerRequestData, timeout?: number ): Promise<any>;
 	protected async _send( ...args: any[] ): Promise<any> {
-		const target: Electron.IpcRenderer|Electron.WebContents = ( typeof args[0] ==="string" ) ? this.electronIPC as Electron.IpcRenderer : args.shift() as Electron.WebContents ;
+		const target: Electron.IpcRenderer|Electron.WebContents = ( typeof args[0] ==="string" ) ? this.#electronIPC as Electron.IpcRenderer : args.shift() as Electron.WebContents ;
 		const label: string = args.shift() as string;
 		const data: IPCMessengerRequestData = args.shift();
 		const timeout: number = ( typeof args[0] === "number" ) ? args.shift() : this.defaultTimeout;
 
-		const id = ( this.waiting.count++ );
+		const id = ( this.#waiting.count++ );
 		// const payload = { data: JSON.stringify( data ), id, label, };
-		const payload = { data, id, label, };
+		const payload: IPCMessengerArg = { data, id, label, };
 
 		target.send( this.channel, payload );
 		this.debug && this.console.debug( `IPCMessenger.send( ${id}, "${label}", ${JSON.stringify(data)} )` );
 
 
-		return new Promise( (resolve: ((value:any)=>void), reject: ((reason?:any)=>void) ) => {
-			if( timeout == 0 ) {
-				return resolve;
-			}
-			const timer = setTimeout( () => {
-				this.cleanUpAfterMessage( id );
-				this.console.error( `IPCMessenger.timeout( ${id}, "${label}", ${JSON.stringify(data)} )` );
-			}, timeout);
+		return new Promise<any>( (resolve: ((value:any)=>void), reject: ((reason?:any)=>void) ) => {
+			const timer = ( timeout > 0 )
+				? setTimeout( () => {
+						this.#cleanUpAfterMessage( id );
+						this.console.error( `IPCMessenger.timeout( ${id}, "${label}", ${JSON.stringify(data)} )` );
+					}, timeout)
+				: undefined
+			;
 
 			let destroyListener: ( ()=>void )|undefined;
-			if( this.electronIPC !== target ) {
-				const listener: ( () => void ) = () => { this.cleanUpAfterMessage( id ); };
+			if( this.#electronIPC !== target ) {
+				const listener: ( () => void ) = () => { this.#cleanUpAfterMessage( id ); };
 				( target as Electron.WebContents ).on( "destroyed", listener );
 				destroyListener = () => { 
 					if( target ) {
@@ -136,7 +136,7 @@ class AbstractIPCMessenger extends EventEmitter {
 				}; 
 			}
 
-			this.waiting.entries[ id ] = { destroyListener, reject, resolve, timer, };
+			this.#waiting.entries[ id ] = { destroyListener, reject, resolve, timer, };
 		} );
 	}
 	
